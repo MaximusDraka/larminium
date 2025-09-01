3d16d0e7da57c3d1e3d2d3a617edd90c7acc3e89
MaximusDraka <tom@larminier.eu>
2025-09-01 21:04:41 +0200

    mdbootstrap included with templates

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World, I\'m Larminium'
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("index.html", title="About")
+
+@app.route("/contact")
+def contact():
+    return render_template("index.html", title="Contact")
+
+if __name__ == "__main__":
+    app.run(debug=True)
