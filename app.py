12670004811d1a49d6da97a3bb1290e0283169f3
MaximusDraka <tom@larminier.eu>
2025-09-01 21:56:48 +0200

    Homepage and contact page content

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("index.html", title="About")
+@app.route("/articles")
+def articles():
+    return render_template("articles.html", title="Articles")
 
 @app.route("/contact")
 def contact():
-    return render_template("index.html", title="Contact")
+    return render_template("contact.html", title="Contact")
 
 if __name__ == "__main__":
     app.run(debug=True)
